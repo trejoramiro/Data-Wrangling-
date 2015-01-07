262f035422f23a951b156f397b97e2abed34b8b5
Ramiro Trejo <trejoramiro@yahoo.com>
2015-01-07 04:22:14 -0600

    Remove comment

--- simple.py
+++ simple.py
@@ -31,7 +31,6 @@ def parse_file(datafile):
 
 
 def test():
-    # a simple test of your implemetation
     datafile = os.path.join(DATADIR, DATAFILE)
     d = parse_file(datafile)
     firstline = {'Title': 'Please Please Me', 'UK Chart Position': '1', 'Label': 'Parlophone(UK)', 'Released': '22 March 1963', 'US Chart Position': '-', 'RIAA Certification': 'Platinum', 'BPI Certification': 'Gold'}
